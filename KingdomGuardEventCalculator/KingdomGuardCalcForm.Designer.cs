76c2f851ce501c8eb9a809e0608e63af9ccee685
Necirvan Taysun <taysun_necirvan@hotmail.com>
2023-10-17 13:46:12 +0200

    * Added calculations for hero phase

--- KingdomGuardEventCalculator/KingdomGuardCalcForm.Designer.cs
+++ KingdomGuardEventCalculator/KingdomGuardCalcForm.Designer.cs
@@ -35,9 +35,26 @@
             Gear_Tab = new TabPage();
             Dragon_Tab = new TabPage();
             Hero_Tab = new TabPage();
-            button1 = new Button();
-            textBox1 = new TextBox();
+            SSR_SumBox = new TextBox();
+            SR_SumBox = new TextBox();
+            R_SumBox = new TextBox();
+            N_SumBox = new TextBox();
+            SSR_NumericBox = new NumericUpDown();
+            R_NumericBox = new NumericUpDown();
+            SR_NumericBox = new NumericUpDown();
+            N_NumericBox = new NumericUpDown();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            calcBtn = new Button();
+            sumAll_Box = new TextBox();
             tabControl1.SuspendLayout();
+            Hero_Tab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)SSR_NumericBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)R_NumericBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SR_NumericBox).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)N_NumericBox).BeginInit();
             SuspendLayout();
             // 
             // tabControl1
@@ -105,28 +122,145 @@
             // Hero_Tab
             // 
             Hero_Tab.BackColor = SystemColors.ControlDark;
+            Hero_Tab.Controls.Add(SSR_SumBox);
+            Hero_Tab.Controls.Add(SR_SumBox);
+            Hero_Tab.Controls.Add(R_SumBox);
+            Hero_Tab.Controls.Add(N_SumBox);
+            Hero_Tab.Controls.Add(SSR_NumericBox);
+            Hero_Tab.Controls.Add(R_NumericBox);
+            Hero_Tab.Controls.Add(SR_NumericBox);
+            Hero_Tab.Controls.Add(N_NumericBox);
+            Hero_Tab.Controls.Add(label4);
+            Hero_Tab.Controls.Add(label3);
+            Hero_Tab.Controls.Add(label2);
+            Hero_Tab.Controls.Add(label1);
             Hero_Tab.Location = new Point(4, 24);
             Hero_Tab.Name = "Hero_Tab";
             Hero_Tab.Size = new Size(768, 369);
             Hero_Tab.TabIndex = 5;
             Hero_Tab.Text = "Hero";
             // 
-            // button1
+            // SSR_SumBox
             // 
-            button1.Location = new Point(713, 415);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 1;
-            button1.Text = "Calculate";
-            button1.UseVisualStyleBackColor = true;
+            SSR_SumBox.Location = new Point(174, 130);
+            SSR_SumBox.Name = "SSR_SumBox";
+            SSR_SumBox.ReadOnly = true;
+            SSR_SumBox.Size = new Size(100, 23);
+            SSR_SumBox.TabIndex = 11;
             // 
-            // textBox1
+            // SR_SumBox
             // 
-            textBox1.Location = new Point(564, 416);
-            textBox1.Name = "textBox1";
-            textBox1.ReadOnly = true;
-            textBox1.Size = new Size(143, 23);
-            textBox1.TabIndex = 2;
+            SR_SumBox.Location = new Point(174, 91);
+            SR_SumBox.Name = "SR_SumBox";
+            SR_SumBox.ReadOnly = true;
+            SR_SumBox.Size = new Size(100, 23);
+            SR_SumBox.TabIndex = 10;
+            // 
+            // R_SumBox
+            // 
+            R_SumBox.Location = new Point(174, 53);
+            R_SumBox.Name = "R_SumBox";
+            R_SumBox.ReadOnly = true;
+            R_SumBox.Size = new Size(100, 23);
+            R_SumBox.TabIndex = 9;
+            // 
+            // N_SumBox
+            // 
+            N_SumBox.Location = new Point(174, 13);
+            N_SumBox.Name = "N_SumBox";
+            N_SumBox.ReadOnly = true;
+            N_SumBox.Size = new Size(100, 23);
+            N_SumBox.TabIndex = 8;
+            // 
+            // SSR_NumericBox
+            // 
+            SSR_NumericBox.Location = new Point(48, 131);
+            SSR_NumericBox.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            SSR_NumericBox.Name = "SSR_NumericBox";
+            SSR_NumericBox.Size = new Size(120, 23);
+            SSR_NumericBox.TabIndex = 7;
+            SSR_NumericBox.ValueChanged += SSR_NumericBox_ValueChanged;
+            // 
+            // R_NumericBox
+            // 
+            R_NumericBox.Location = new Point(48, 53);
+            R_NumericBox.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            R_NumericBox.Name = "R_NumericBox";
+            R_NumericBox.Size = new Size(120, 23);
+            R_NumericBox.TabIndex = 6;
+            R_NumericBox.ValueChanged += R_NumericBox_ValueChanged;
+            // 
+            // SR_NumericBox
+            // 
+            SR_NumericBox.Location = new Point(48, 92);
+            SR_NumericBox.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            SR_NumericBox.Name = "SR_NumericBox";
+            SR_NumericBox.Size = new Size(120, 23);
+            SR_NumericBox.TabIndex = 5;
+            SR_NumericBox.ValueChanged += SR_NumericBox_ValueChanged;
+            // 
+            // N_NumericBox
+            // 
+            N_NumericBox.Location = new Point(48, 14);
+            N_NumericBox.Maximum = new decimal(new int[] { 999999, 0, 0, 0 });
+            N_NumericBox.Name = "N_NumericBox";
+            N_NumericBox.Size = new Size(120, 23);
+            N_NumericBox.TabIndex = 4;
+            N_NumericBox.ValueChanged += N_NumericBox_ValueChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(17, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(26, 15);
+            label4.TabIndex = 3;
+            label4.Text = "SSR";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(20, 15);
+            label3.TabIndex = 2;
+            label3.Text = "SR";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(14, 15);
+            label2.TabIndex = 1;
+            label2.Text = "R";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(16, 15);
+            label1.TabIndex = 0;
+            label1.Text = "N";
+            // 
+            // calcBtn
+            // 
+            calcBtn.Location = new Point(713, 415);
+            calcBtn.Name = "calcBtn";
+            calcBtn.Size = new Size(75, 23);
+            calcBtn.TabIndex = 1;
+            calcBtn.Text = "Calculate";
+            calcBtn.UseVisualStyleBackColor = true;
+            calcBtn.Click += calcBtn_Click;
+            // 
+            // sumAll_Box
+            // 
+            sumAll_Box.Location = new Point(564, 416);
+            sumAll_Box.Name = "sumAll_Box";
+            sumAll_Box.ReadOnly = true;
+            sumAll_Box.Size = new Size(143, 23);
+            sumAll_Box.TabIndex = 2;
             // 
             // KingdomGuardCalcForm
             // 
@@ -134,12 +268,18 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlDarkDark;
             ClientSize = new Size(800, 450);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
+            Controls.Add(sumAll_Box);
+            Controls.Add(calcBtn);
             Controls.Add(tabControl1);
             Name = "KingdomGuardCalcForm";
             Text = "KingdomGuardCalcForm";
             tabControl1.ResumeLayout(false);
+            Hero_Tab.ResumeLayout(false);
+            Hero_Tab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)SSR_NumericBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)R_NumericBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SR_NumericBox).EndInit();
+            ((System.ComponentModel.ISupportInitialize)N_NumericBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -149,11 +289,23 @@
         private TabControl tabControl1;
         private TabPage Unit_Tab;
         private TabPage Summon_Tab;
-        private Button button1;
+        private Button calcBtn;
         private TabPage Witch_Tab;
         private TabPage Gear_Tab;
         private TabPage Dragon_Tab;
         private TabPage Hero_Tab;
-        private TextBox textBox1;
+        private TextBox sumAll_Box;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private NumericUpDown SSR_NumericBox;
+        private NumericUpDown R_NumericBox;
+        private NumericUpDown SR_NumericBox;
+        private NumericUpDown N_NumericBox;
+        private TextBox SSR_SumBox;
+        private TextBox SR_SumBox;
+        private TextBox R_SumBox;
+        private TextBox N_SumBox;
     }
 }
--- KingdomGuardEventCalculator/KingdomGuardCalcForm.cs
+++ KingdomGuardEventCalculator/KingdomGuardCalcForm.cs
@@ -1,20 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace KingdomGuardEventCalculator
+﻿namespace KingdomGuardEventCalculator
 {
     public partial class KingdomGuardCalcForm : Form
     {
+        private long NValue = 0;
+        private long RValue = 0;
+        private long SRValue = 0;
+        private long SSRValue = 0;
+
         public KingdomGuardCalcForm()
         {
             InitializeComponent();
+        }
+
+        private void N_NumericBox_ValueChanged(object sender, EventArgs e)
+        {
+            NValue = (long)N_NumericBox.Value;
+            this.UpdateSumBox(NValue, N_SumBox, 100);
+        }
+
+        private void R_NumericBox_ValueChanged(object sender, EventArgs e)
+        {
+            RValue = (long)R_NumericBox.Value;
+            this.UpdateSumBox(RValue, R_SumBox, 700);
+        }
+
+        private void SR_NumericBox_ValueChanged(object sender, EventArgs e)
+        {
+            SRValue = (long)SR_NumericBox.Value;
+            this.UpdateSumBox(SRValue, SR_SumBox, 3500);
+        }
+
+        private void SSR_NumericBox_ValueChanged(object sender, EventArgs e)
+        {
+            SSRValue = (long)SSR_NumericBox.Value;
+            this.UpdateSumBox(SSRValue, SSR_SumBox, 14000);
+        }
+
+        public void UpdateSumBox(long numericBoxValue, TextBox sumBox, int points)
+        {
+            var sum = numericBoxValue * points;
+            sumBox.Text = sum.ToString("N0");
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            var sum = NValue * 100 + RValue * 700 + SRValue * 3500 + SSRValue * 14000;  
+
+            sumAll_Box.Text = sum.ToString("N0");
         }
     }
 }
